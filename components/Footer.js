b12c4d7fadd5052a11750865004955bc9dbb11ed
azteck <azteck.indian@gmail.com>
2022-09-29 16:21:53 +0530

    INITIAL - Day Two and Three

---
+++ components/Footer.js
@@ -0,0 +1,40 @@
+function Footer() {
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-4 gap-y-10 px-32 py-14 bg-gray-100 text-xs text-gray-800">
+      <div className="space-y-4 ">
+        <h5 className="font-bold">ABOUT</h5>
+        <p>How Airbnb works</p>
+        <p>Newsroom</p>
+        <p>Investors</p>
+        <p>Airbnb Plus</p>
+        <p>Airbnb Luxe</p>
+      </div>
+      <div className="space-y-4">
+        <h5 className="font-bold">COMMUNITY</h5>
+        <p>Accessibility</p>
+        <p>This is not a real site</p>
+        <p>It&apos;s a pretty awesome clone</p>
+        <p>Referrals accepted</p>
+        <p>Social</p>
+      </div>
+      <div className="space-y-4">
+        <h5 className="font-bold">HOST</h5>
+        <p>Airbnb</p>
+        <p>Presents</p>
+        <p>Experinces</p>
+        <p>To cherish and</p>
+        <p>Celebrate adventure</p>
+      </div>
+      <div className="space-y-4">
+        <h5 className="font-bold">SUPPORT</h5>
+        <p>Help Center</p>
+        <p>Trust & Safety</p>
+        <p>Say hi</p>
+        <p>Holiay specials</p>
+        <p>Lucky draw</p>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
